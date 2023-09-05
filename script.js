d667e4d1f8b24eeacf1e4a37b0d91b2f0b834919
ShafayetAhmad <shafayet.ahmad1@gmail.com>
2023-09-05 13:42:28 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function convertTime() {
+  const fromTimeZone = document.getElementById("fromTimeZone").value;
+  const toTimeZone = document.getElementById("toTimeZone").value;
+  const timeInput = document.getElementById("timeInput").value;
+
+  const fromTime = new Date(`1970-01-01T${timeInput}Z`);
+  const toTime = new Date(
+    fromTime.toLocaleString("en-US", { timeZone: toTimeZone })
+  );
+
+  const resultElement = document.getElementById("result");
+  resultElement.textContent = `Converted Time: ${toTime.toLocaleTimeString()}`;
+}
